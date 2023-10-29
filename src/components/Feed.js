3f47bbc554562ae74b371cb9029d60925bc7064a
Durrell Jules <dujules23@gmail.com>
2023-10-28 22:31:51 -0400

    added posts section of the feed component, rendered succufully

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,7 +1,29 @@
 import { SparklesIcon } from "@heroicons/react/24/outline";
 import Input from "./Input";
+import Post from "./Post";
 
 export default function Feed() {
+  const posts = [
+    {
+      id: "1",
+      name: "Durrell Jules",
+      username: "percivalwright",
+      userImg: "https://durrell-portfolio.vercel.app/portrait.jpg",
+      img: "https://images.unsplash.com/photo-1697446274337-1abda37787cf?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDF8SnBnNktpZGwtSGt8fGVufDB8fHx8fA%3D%3D",
+      text: "A unique animal",
+      timestamp: "2 hours ago",
+    },
+    {
+      id: "2",
+      name: "Durrell Jules",
+      username: "percivalwright",
+      userImg: "https://durrell-portfolio.vercel.app/portrait.jpg",
+      img: "https://images.unsplash.com/photo-1697906943377-48090b527d91?auto=format&fit=crop&q=60&w=500&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDEwfEpwZzZLaWRsLUhrfHxlbnwwfHx8fHw%3D",
+      text: "Whooooo do you think you are?",
+      timestamp: "4 days ago",
+    },
+  ];
+
   return (
     <div className="xl:ml-[370px] border-l border-r border-gray-200 xl:min-w-[576px] sm:ml-[73px] flex-grow max-w-xl">
       <div className="flex py-2 px-3 sticky top-0 z-50 bg-white border-b border-gray-200">
@@ -11,6 +33,9 @@ export default function Feed() {
         </div>
       </div>
       <Input />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} />
+      ))}
     </div>
   );
 }
