296683d65d667a44450c93d8814e5bca2a4aefa4
Durrell Jules <dujules23@gmail.com>
2023-11-03 21:26:50 -0400

    got the session and added to input.js and sidebar.js

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -4,9 +4,12 @@ import SidebarMenuItem from "./SidebarMenuItem";
 import { iconData } from "@/utils/iconData";
 import { EllipsisHorizontalIcon } from "@heroicons/react/24/solid";
 
+import { useSession, signIn, signOut } from "next-auth/react";
+
 export default function Sidebar() {
   // todo: find a way to make the menu items bold only when selected, since they are mapped there is currently an issue.
 
+  const { data: session } = useSession();
   return (
     <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       {/* Twitter Logo */}
@@ -21,32 +24,46 @@ export default function Sidebar() {
       </div>
 
       {/* Menu */}
-      <div className="mt-4 mb-2.5 xl:item-start">
-        {iconData.map((item, idx) => {
-          const { text, Icon } = item;
-          return <SidebarMenuItem key={idx} text={text} Icon={Icon} active />;
-        })}
-      </div>
-      {/* Button */}
+      {session ? (
+        <>
+          <div className="mt-4 mb-2.5 xl:item-start">
+            {iconData.map((item, idx) => {
+              const { text, Icon } = item;
+              return (
+                <SidebarMenuItem key={idx} text={text} Icon={Icon} active />
+              );
+            })}
+          </div>
+          {/* Button */}
 
-      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
-        Post
-      </button>
+          <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline">
+            Post
+          </button>
 
-      {/* Mini Profile */}
+          {/* Mini Profile */}
 
-      <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
-        <img
-          className="h-10 w-10 rounded-full xl:mr-2"
-          src="https://durrell-portfolio.vercel.app/portrait.jpg"
-          alt="profile-image"
-        />
-        <div className="leading-5 hidden xl:inline">
-          <h4 className="font-bold">Durrell Jules</h4>
-          <p className="text-gray-500">@PercivalWright</p>
-        </div>
-        <EllipsisHorizontalIcon className="h-7 xl:ml-8" />
-      </div>
+          <div className="hoverEffect text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+            <img
+              onClick={signOut}
+              className="h-10 w-10 rounded-full xl:mr-2"
+              src={session.user.image}
+              alt="profile-image"
+            />
+            <div className="leading-5 hidden xl:inline">
+              <h4 className="font-bold">{session.user.name}</h4>
+              <p className="text-gray-500">@{session.user.username}</p>
+            </div>
+            <EllipsisHorizontalIcon className="h-7 xl:ml-8" />
+          </div>
+        </>
+      ) : (
+        <button
+          onClick={signIn}
+          className="mt-12 bg-blue-400 text-white rounded-full w-36 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline"
+        >
+          Sign in
+        </button>
+      )}
     </div>
   );
 }
