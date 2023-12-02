2257dc7242d067e875ec7e4c7c8b638976d43838
Durrell Jules <dujules23@gmail.com>
2023-12-01 23:09:48 -0500

    installed react-modal, setup basic modal

--- src/components/CommentModal.js
+++ src/components/CommentModal.js
@@ -1,12 +1,20 @@
 import { useRecoilState } from "recoil";
 import { modalState } from "../../atom/modalAtom";
+import Modal from "react-modal";
 
 export default function CommentModal() {
   const [open, setOpen] = useRecoilState(modalState);
   return (
     <div>
-      <h1>CommentModal</h1>
-      {open && <h1>The modal is open</h1>}
+      {open && (
+        <Modal
+          isOpen={open}
+          onRequestClose={() => setOpen(false)}
+          className="max-w-lg w-[90%] h-[300px] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray- 400 rounded-xl shadow-md"
+        >
+          <h1>Comment Modal</h1>
+        </Modal>
+      )}
     </div>
   );
 }
