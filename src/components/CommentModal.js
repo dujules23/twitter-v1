b088c5f8697c4164a1ea3203b68ce2489ac5e5a7
Durrell Jules <dujules23@gmail.com>
2023-12-03 21:13:19 -0500

    finalized layout of CommentModal

--- src/components/CommentModal.js
+++ src/components/CommentModal.js
@@ -1,7 +1,7 @@
 import { useRecoilState } from "recoil";
 import { modalState, postIdState } from "../../atom/modalAtom";
 import Modal from "react-modal";
-import { XMarkIcon } from "@heroicons/react/24/solid";
+import { XMarkIcon, PhotoIcon, FaceSmileIcon } from "@heroicons/react/24/solid";
 import { useEffect, useState } from "react";
 import { db } from "../../firebase";
 import { doc, onSnapshot } from "firebase/firestore";
@@ -12,7 +12,10 @@ export default function CommentModal() {
   const [open, setOpen] = useRecoilState(modalState);
   const [postId] = useRecoilState(postIdState);
   const [post, setPost] = useState({});
+  const [input, setInput] = useState("");
   const { data: session } = useSession();
+
+  const sendComment = () => {};
 
   useEffect(() => {
     onSnapshot(doc(db, "posts", postId), (snapshot) => {
@@ -26,7 +29,7 @@ export default function CommentModal() {
         <Modal
           isOpen={open}
           onRequestClose={() => setOpen(false)}
-          className="max-w-lg w-[90%] h-[300px] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-400 rounded-xl shadow-md"
+          className="max-w-lg w-[90%] absolute top-24 left-[50%] translate-x-[-50%] bg-white border-2 border-gray-400 rounded-xl shadow-md"
         >
           <div className="p-1">
             <div className="border-b border-gray-200 py-2 px-1.5">
@@ -54,6 +57,54 @@ export default function CommentModal() {
               <span className="text-sm sm:text-[15px] hover:underline">
                 <Moment fromNow>{post?.data()?.timestamp?.toDate()}</Moment>
               </span>
+            </div>
+
+            <p className="text-gray-500 text-[15px] sm:text-[16px] ml-16 mb-2">
+              {post?.data()?.text}
+            </p>
+
+            <div className="flex border-gray-200 p-3 space-x-3">
+              <img
+                src={session.user.image}
+                alt="user-image"
+                className="h-11 w-11 rounded-full cursor-pointer hover:brightness-95"
+              />
+              <div className="w-full divide-y divide-gray-200">
+                <div className="">
+                  <textarea
+                    className="w-full border-none focus:ring-0 text-lg placeholder-gray-700 tracking-wide min-h-[50px] text-gray-700"
+                    rows="2"
+                    placeholder="Post your reply"
+                    value={input}
+                    onChange={(e) => setInput(e.target.value)}
+                  ></textarea>
+                </div>
+
+                <div className="flex items-center justify-between pt-2.5">
+                  <div className="flex">
+                    <div
+                      className=""
+                      onClick={() => filePickerRef.current.click()}
+                    >
+                      <PhotoIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                      {/* <input
+                        type="file"
+                        hidden
+                        ref={filePickerRef}
+                        onChange={addImageToPost}
+                      /> */}
+                    </div>
+                    <FaceSmileIcon className="h-10 w-10 hoverEffect p-2 text-sky-500 hover:bg-sky-100" />
+                  </div>
+                  <button
+                    onClick={sendComment}
+                    disabled={!input.trim()}
+                    className="bg-blue-400 text-white px-4 py-1.5 rounded-full font-bold shadow-md hover:brightness-95 disabled:opacity-50"
+                  >
+                    Post
+                  </button>
+                </div>
+              </div>
             </div>
           </div>
         </Modal>
